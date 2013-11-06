79bcd991ece2c88378b2741c1525a29a2b331fbb
liz rush <liz@lizmrush.com>
2013-11-06 13:53:59 -0800

    changes to basic objects

--- examples/week2/basic_objects/baker.rb
+++ examples/week2/basic_objects/baker.rb
@@ -1,19 +1,43 @@
-class Baker
-  attr_accessor :eggs, :butter, :rolling_pin
+require "./oven.rb"
 
-  def initialize(rolling_pin_variable)
+class Baker
+  attr_accessor :rolling_pin, :apron, :oven
+  attr_reader :eggs, :butter, :sugar, :flour
+
+  def initialize(options_hash)
     @flour  = 2
     @butter = 5
     @sugar  = 3
     @eggs   = 23
-    @rolling_pin = rolling_pin_variable
+    @rolling_pin = options_hash[:rolling_pin]
+    @apron = options_hash[:apron]
+    @ovens = options_hash[:ovens]
   end
 
   def make_cookies
-    heat_oven(350)
+    heat_oven(@ovens[0], temperature)
+    # otehr steps in recipe
   end
 
-  def heat_oven(temperature)
+  def heat_oven(baking_oven, temperature)
+    baking_oven.set_temperature(temperature)
     puts "The oven is heating up to #{temperature}"
   end
+
+  def turn_off_ovens
+    # ovens_that_are_on = @ovens.select { |oven| oven.on == true }
+    # # returns array
+    # ovens_that_are_on.each do |oven|
+    #   oven.on = false
+    # end
+
+    #simpler method
+    @ovens.each do |oven|
+      # if oven.on == true  #not necessary here because we want all off
+        oven.on = false
+      end
+
+    puts "all ovens are off!"
+  end
+
 end
